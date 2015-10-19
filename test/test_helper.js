e8c96a64bbbf6f842ff60941f31ea808f9412278
thtruo <ttruongchau@gmail.com>
2015-10-18 17:25:38 -0700

    added comments

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,3 +1,9 @@
+/* Setup code for jsdom. Create jsdom versions of `document` and `window`
+ * objects that would normally be provided by the browser. Then put them
+ * in the global object so they will be discovered by React when it
+ * accesses `document` or `window`
+ */
+
 import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
@@ -8,6 +14,10 @@ const win = doc.defaultView;
 global.document = doc;
 global.window = win;
 
+/* Hoist all jsdom window object properties to Node's global object so that
+ * properties provided by window can be used without the `window.`-prefix
+ * which is what would happen in a browser environment.
+ */
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
     global[key] = window[key];
