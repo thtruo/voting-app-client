409e84bfed615faa84a433a8e38b7fd5646937ea
thtruo <ttruongchau@gmail.com>
2015-10-21 21:16:05 -0700

    added react-router and configured voting and results paths

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,21 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 require('./style.css');
 
 const pair = ['Batman Begins', 'The Dark Knight'];
 
+const routes = <Route component={App}>
+  <Route path="/results" component={Results}></Route>
+  <Route path="/" component={Voting}></Route>
+</Route>;
+
 ReactDOM.render(
-  <Voting pair={pair}
-          winner="The Dark Knight" />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
